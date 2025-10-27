f9687257367f1f517da50675e26c3cf7dd858736
SzymonMuttka <21278@student.ans-elblag.pl>
2025-10-27 10:58:49 +0100

    Lab4 zad 2.6

--- Lab4ReactDev/my-app/src/App.js
+++ Lab4ReactDev/my-app/src/App.js
@@ -2,6 +2,7 @@ import HelloWithProps from './Hello';
 import Counter from './Counter';
 import InputTracker from './InputTracker';
 import LoginStatus from './LoginStatus';
+import TodoList from './TodoList';
 
 function App(){
   return(
@@ -16,6 +17,8 @@ function App(){
       
       <LoginStatus isLoggedIn={false}/>
       <LoginStatus isLoggedIn={true}/>
+
+      <TodoList todos={['test1', 'test2', 'test3']}/>
     </div>
   );
 }
