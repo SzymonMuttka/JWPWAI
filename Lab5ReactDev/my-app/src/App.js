3931a3aee81d9133f46ea5945619953083a146db
SzymonMuttka <21278@student.ans-elblag.pl>
2025-11-03 10:26:26 +0100

    Lab5 zad 1

---
+++ Lab5ReactDev/my-app/src/App.js
@@ -0,0 +1,13 @@
+import Hello from './Hello';
+import ToggleDetails from './ToggleDetails';
+
+function App(){
+  return(
+    <div>
+      <Hello />
+      <ToggleDetails />
+    </div>
+  );
+}
+
+export default App;
