ad4ec71b6386d7233da0a5cd13eb293fa1e8d7bd
SzymonMuttka <21278@student.ans-elblag.pl>
2025-11-03 10:30:23 +0100

    Lab5 zad 2

--- Lab5ReactDev/my-app/src/App.js
+++ Lab5ReactDev/my-app/src/App.js
@@ -1,11 +1,17 @@
 import Hello from './Hello';
 import ToggleDetails from './ToggleDetails';
+import ScoreDisplay from './ScoreDisplay';
 
 function App(){
   return(
     <div>
       <Hello />
+      
       <ToggleDetails />
+
+      <ScoreDisplay score={0}/>
+      <ScoreDisplay score={60}/>
+      <ScoreDisplay score={120}/>
     </div>
   );
 }
