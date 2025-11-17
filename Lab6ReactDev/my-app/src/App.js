ba35edd5ce6ee581cc272d7ffdb5c8800d50cc3f
SzymonMuttka <21278@student.ans-elblag.pl>
2025-11-17 10:41:16 +0100

    Lab6 zad 1

---
+++ Lab6ReactDev/my-app/src/App.js
@@ -0,0 +1,14 @@
+import Hello from './Hello';
+import ClickCounter from './ClickCounter';
+
+function App() {
+  return (
+    <div>
+      <Hello />
+
+      <ClickCounter />
+    </div>
+  );
+}
+
+export default App;
---
+++ Lab6ReactDev/my-app/src/Hello.js
@@ -0,0 +1,9 @@
+function Hello() {
+    return (
+        <div>
+            Hello world!
+        </div>
+    )
+}
+
+export default Hello;
