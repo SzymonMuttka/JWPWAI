ff3dcc92ac30c81ab6b683bbb346f51777182b94
SzymonMuttka <21278@student.ans-elblag.pl>
2025-11-17 11:08:57 +0100

    Lab6 zad 4

--- Lab6ReactDev/my-app/src/App.js
+++ Lab6ReactDev/my-app/src/App.js
@@ -2,6 +2,16 @@ import Hello from './Hello';
 import ClickCounter from './ClickCounter';
 import PrimeCalculator from './PrimeCalculator';
 import FormReducer from './FormReducer';
+import { useTheme, ThemeProvider } from './ThemeContext';
+
+function ThemeSwitcher() {
+  const { dark, toggleTheme } = useTheme()
+  return (
+    <div style={{ background: dark ? "#333" : "#fff", color: dark ? "#fff" : "#000" }}>
+      <button onClick={toggleTheme}>Przełącz motyw</button>
+  </div>
+  )
+}
 
 function App() {
   return (
@@ -13,6 +23,10 @@ function App() {
       <PrimeCalculator />
 
       <FormReducer />
+
+      <ThemeProvider>
+        <ThemeSwitcher />
+      </ThemeProvider>
     </div>
   );
 }
